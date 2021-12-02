d4de1bfc4675ca0f84c222f9d35193426fbf12a0
shounak1 <shounakbhandekar96@gmail.com>
2021-12-02 03:20:15 +0000

    Followed tutorial from 26th October, 2021

--- contract.js
+++ contract.js
@@ -1,3 +1,312 @@
 const Web3 = require("web3")
 
-console.log("hello World");
+// https://ropsten.infura.io/v3/
+
+// today's lab
+
+// instiantiate web3
+const rpcURL = "https://ropsten.infura.io/v3/"
+
+const web3 = new Web3(rpcURL);
+console.log("Connected to web3");
+
+const abi = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "_totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "tokenOwner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "recipient",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "sender",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "recipient",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	}
+]
+// get abi interface for our contract
+//connect to our contract on ropsten
+
+// get our contract address
+const address = "0x6e065b83d48fa2a779b181a2352f825e5b3e7c7d";
+const owner = "0x8f9d710C41e926A7757C10CCebDeD2d47601B4c2";
+
+const contract = new web3.eth.Contract(abi, address);
+console.log("connected to the contract on ropsten");
+// run some of the methods in our contract (using javascript)
+
+const getTotalSupply = async() => {
+    let totSupply = await contract.methods.totalSupply().call();
+    return "Total supply is : " + totSupply;
+}
+
+const getSymbol = async() => {
+    let symbol = await contract.methods.symbol().call();
+    return "Symbol is : " + symbol;
+}
+
+const getBalanceOfOwner = async() => {
+    let bal = await contract.methods.balanceOf(owner).call();
+    return "Balance of Owner : " + bal;
+}
+
+const getName = async() => {
+    let name = await contract.methods.name().call();
+    return "Name of the owner is : " + name;
+}
+
+const getDecimals = async() => {
+    let decimal = await contract.methods.decimals().call();
+    return "Decimals : " + decimal;
+}
+
+const returnAllValues = async() => {
+    console.log(await getTotalSupply());
+    console.log(await getSymbol());
+    console.log(await getBalanceOfOwner());
+    console.log(await getDecimals());
+    console.log(await getName());
+}
+
+returnAllValues();
---
+++ method.js
@@ -0,0 +1,7 @@
+// connect to our contract
+
+// create a transaction to execute a method on our contract
+
+// sign the transaction with our private key
+
+// broadcast our transaction
